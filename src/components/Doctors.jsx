875ba050eaa4971b201b2c783c704102b82f1d95
Pritam <pritamdas5370@gmail.com>
2024-10-05 17:11:48 +0530

    44

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -18,18 +18,24 @@ const Doctors = () => {
   return (
     <div className=" px-[5vw] py-16">
       <h1 className=" sm:text-4xl text-3xl font-[bolld] font-semibold text-center">
-        Oska was developed in collaboration <br className="sm:block hidden" /> with leading medical experts
+        Cuore is developed in collaboration <br className="sm:block hidden" />{" "}
+        with leading medical experts
       </h1>
 
       <div className=" flex sm:flex-row flex-col justify-center gap-5 mt-10">
         {doctor.map((item, index) => {
           return (
-            <div key={index} className=" p-7 sm:h-[25vw] w-full h-[50vh] sm:w-[22vw] rounded-2xl bg-zinc-100 flex justify-center items-center">
+            <div
+              key={index}
+              className=" p-7 sm:h-[25vw] w-full h-[50vh] sm:w-[22vw] rounded-2xl bg-zinc-100 flex justify-center items-center"
+            >
               <div>
                 <h1 className=" text-center text-2xl font-semibold font-[bolld]">
                   {item.name}
                 </h1>
-                <p className="mt-3 text-center text-xl font-medium">{item.domain}</p>
+                <p className="mt-3 text-center text-xl font-medium">
+                  {item.domain}
+                </p>
               </div>
             </div>
           );
