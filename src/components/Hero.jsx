c9be226d0dc6b2c8c5d492967fbe61525ae2ce33
Pritam <pritamdas5370@gmail.com>
2024-10-08 21:34:16 +0530

    65

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,9 +11,9 @@ const Hero = () => {
         </div>
 
         <div className=" lg:flex items-center gap-10 text-[1.05vw] font-bold lg:visible hidden">
-          <p>Individuals</p>
+          {/* <p>Individuals</p>
           <p>Medical professionals</p>
-          <p>How it works</p>
+          <p>How it works</p> */}
           <p>About us</p>
           <p>Contact us</p>
         </div>
@@ -25,9 +25,9 @@ const Hero = () => {
 
       <div className={`h-fit rounded-2xl w-full absolute top-0 right-0 bg-zinc-100 shadow-2xl z-[899] flex flex-col gap-10 sm:hidden items-start px-5 py-10 text-xl transform ${open ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-500`}>
         <div className=" w-full flex justify-end text-xl"><i onClick={()=>setOpen(false)} className="ri-close-line"></i></div>
-        <p>Individuals</p>
+        {/* <p>Individuals</p>
         <p>Medical professionals</p>
-        <p>How it works</p>
+        <p>How it works</p> */}
         <p>About us</p>
         <p>Contact us</p>
       </div>
