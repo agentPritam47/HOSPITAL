55ecfa22c29c475ce665a36dab0989bfc771a6fb
Pritam <pritamdas5370@gmail.com>
2024-10-08 22:46:09 +0530

    85

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
           OSKA
         </h1> */}
         <div className=" w-full h-fit sm:pb-5 flex items-center justify-center sm:justify-start">
-          <div className=" sm:w-[14vw] sm:h-[5vw] w-[14vh] h-[5vh] flex items-center justify-center">
+          <div className=" sm:w-[14vw] sm:h-[5vw] w-[14vh] h-[5vh] flex items-center justify-center sm:ml-[-2vw]">
             <img src="Logoo-removebg-preview.png" alt="" />
           </div>
         </div>
