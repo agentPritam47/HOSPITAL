f22e5ea9e7d5b90ee1cba4bcf654dba6f7686657
Pritam <pritamdas5370@gmail.com>
2024-10-08 21:05:16 +0530

    59

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,28 +9,16 @@ const Footer = () => {
           OSKA
         </h1>
         <div className=" flex sm:flex-row items-start flex-col mt-10 justify-between">
-          <div className=" sm:w-[40%] text-center sm:text-left w-full">
+          <div className=" sm:w-[30%] text-center sm:text-left w-full">
             <p className=" text-2xl opacity-80 text-white">
-              info@oska-health.com
-            </p>
-            <p className=" text-2xl opacity-80 text-white mt-3">
-              info@oska-health.com
-            </p>
-            <p className=" text-2xl opacity-80 text-white mt-3">
-              info@oska-health.com
+              info@cuore.today
             </p>
           </div>
-          <div className=" sm:w-[20%] w-full flex flex-col items-center mt-10 sm:mt-0">
-            <p className=" text-xl font-semibold">We are here for you</p>
-            <p className=" text-xl opacity-80 text-white mt-3">Patients</p>
-            <p className=" text-xl opacity-80 text-white mt-3">Patients</p>
-            <p className=" text-xl opacity-80 text-white mt-3">Patients</p>
-          </div>
+          
           <div className=" sm:w-[20%] w-full flex flex-col items-center mt-10 sm:mt-0">
             <p className=" text-xl font-semibold">About Oska</p>
             <p className=" text-xl opacity-80 text-white mt-3">About Us</p>
-            <p className=" text-xl opacity-80 text-white mt-3">Leadership</p>
-            <p className=" text-xl opacity-80 text-white mt-3">vaccencies</p>
+            <p className=" text-xl opacity-80 text-white mt-3">Contact Us</p>
           </div>
           <div className=" sm:w-[20%] w-full flex flex-col items-center mt-10 sm:mt-0">
             <p className=" text-xl font-semibold">Legal</p>
