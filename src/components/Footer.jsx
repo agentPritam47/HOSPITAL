c9804963c4cb31e38069d256d805a72a7ef41241
Pritam <pritamdas5370@gmail.com>
2024-10-08 22:40:29 +0530

    83

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 
-
 const Footer = () => {
   return (
     <div className=" pt-10 font-[bolld]">
-      <div className="  w-full sm:h-[fit] sm:pb-[10vw] bg-blue-800 sm:px-[10vw] py-[5vw] text-white">
-        <h1 className=" text-5xl text-white text-center sm:text-left sm:pb-5 ">
+      <div className="  w-full sm:h-[fit] sm:pb-[10vw] bg-blue-400 sm:px-[10vw] py-[5vw] text-white">
+        {/* <h1 className=" text-5xl text-white text-center sm:text-left sm:pb-5 ">
           OSKA
-        </h1>
+        </h1> */}
+        <div className=" w-full h-fit sm:pb-5 flex items-center justify-center">
+          <div className=" sm:w-[14vw] sm:h-[5vw] w-[14vh] h-[5vh] flex items-center justify-center">
+            <img src="Logoo-removebg-preview.png" alt="" />
+          </div>
+        </div>
         <div className=" flex sm:flex-row items-start flex-col mt-10 justify-between">
           <div className=" sm:w-[30%] text-center sm:text-left w-full">
-            <p className=" text-2xl opacity-80 text-white">
-              info@cuore.today
-            </p>
+            <p className=" text-2xl opacity-80 text-white">info@cuore.today</p>
           </div>
-          
+
           <div className=" sm:w-[20%] w-full flex flex-col items-center mt-10 sm:mt-0">
-            <p className=" text-xl font-semibold">About Oska</p>
-            <p className=" text-xl opacity-80 text-white mt-3">About Us</p>
-            <p className=" text-xl opacity-80 text-white mt-3">Contact Us</p>
+            <p className=" text-xl font-semibold">About Cuore</p>
+            <p className=" text-xl opacity-80 text-white mt-3">About us</p>
+            <p className=" text-xl opacity-80 text-white mt-3">Contact us</p>
           </div>
           <div className=" sm:w-[20%] w-full flex flex-col items-center mt-10 sm:mt-0">
             <p className=" text-xl font-semibold">Legal</p>
