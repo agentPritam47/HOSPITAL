6f6057b9fb89d0382f55d6bd32ebe935f9d5b05b
Pritam <pritamdas5370@gmail.com>
2024-10-08 19:50:02 +0530

    55

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -227,17 +227,17 @@ const Info = () => {
           <div className=" flex-shrink-0 sm:h-[25vw] h-[30vh] w-[25vh] rounded-2xl sm:w-[20vw] bg-red-100 relative">
             <div
               ref={fe1}
-              className=" flex p-5 items-center justify-center opacity-0 sm:h-[6vw] h-[6vh] w-[90%] rounded-2xl bg-zinc-50 shadow-xl absolute top-[10%] left-[40%] -translate-x-1/2"
+              className=" flex px-3 items-center justify-center opacity-0 sm:h-[6vw] h-[6vh] w-[90%] rounded-2xl bg-zinc-50 shadow-xl absolute top-[10%] left-[40%] -translate-x-1/2"
             ><h1 className=" text-xs sm:text-lg">Reduce Blood Pressure by ~14 mmHG1</h1></div>
             <div
               ref={fe2}
-              className="flex p-5 items-center justify-center opacity-0 sm:h-[6vw] h-[6vh] w-[90%] rounded-2xl bg-zinc-50 shadow-xl absolute top-[40%] left-[40%] -translate-x-1/2"
+              className="flex px-3 items-center justify-center opacity-0 sm:h-[6vw] h-[6vh] w-[90%] rounded-2xl bg-zinc-50 shadow-xl absolute top-[40%] left-[40%] -translate-x-1/2"
             >
               <h1 className=" text-xs sm:text-lg">Reduce Blood Sugar by ~30 mg/dl2</h1>
             </div>
             <div
               ref={fe3}
-              className="flex p-5 items-center justify-center opacity-0 sm:h-[6vw] h-[6vh] w-[90%] rounded-2xl bg-zinc-50 shadow-xl absolute top-[70%] left-[40%] -translate-x-1/2"
+              className="flex px-3 items-center justify-center opacity-0 sm:h-[6vw] h-[6vh] w-[90%] rounded-2xl bg-zinc-50 shadow-xl absolute top-[70%] left-[40%] -translate-x-1/2"
             >
               <h1 className=" text-xs sm:text-lg">Reduce cardiac risk by ~50%3</h1>
             </div>
